7f95e8138e3a0eb6dd86802c95af7a75d66e5239
Shubhangi Shrestha <shubhangish38@gmail.com>
2023-10-01 22:49:26 +0530

    Another commit in branch3

--- branch3.js
+++ branch3.js
@@ -1,3 +1,5 @@
 console.log("Hey I'm branch3");
 console.log(`I'm here to show the difference between merge and rebase and
 I will support rebase`);
+//it is be second commit of branch3
+console.log("I am second for branch3");
